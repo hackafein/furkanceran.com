ed8a73ccfe8e9ededb3fe183a76a41662a4978b5
hackafein <hackafein@gmail.com>
2020-10-05 14:13:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def server_error(e):
 def return_file():
     return send_file(YUKLEME_KLASORU+"/CV.pdf")
 
-@app.route("/sitemap")
+@app.route("/sitemap.xml")
 def sitemap():
         return render_template("/sitemap.xml")
 @app.route("/test")
